5c4d41e24b27a7b72593ccbd61a44df625aa06de
daniel <bejtrup@gmail.com>
2018-03-13 21:15:22 +0100

    stuff

---
+++ js/ratebands.js
@@ -0,0 +1,34 @@
+$(function(){
+  $("div.bandName").html(getBandNameHtml(0)).removeClass("out");
+  var isAnimationOn = false;
+  $("div.food").click(function(){
+    if(!isAnimationOn){
+      $this = $(this);
+      $this.addClass("fall");
+      isAnimationOn = true;
+      setTimeout(function(){
+        $this.removeClass("fall");
+        isAnimationOn = false;
+        loadNextBand();
+      }, 1200);
+    }
+  });
+});
+
+function rateBand(){
+
+}
+function loadNextBand(){
+  $("div.bandName").addClass("out");
+  setTimeout(function(){
+    var id = $("div.bandName input").val();
+    id = parseInt(id)+1
+    $("div.bandName").html(getBandNameHtml( id ));
+    $("div.bandName").removeClass("out");
+  }, 700);
+}
+function getBandNameHtml(id){
+  var name = bands[id].name;
+  var day = bands[id].fullDate;
+  return "<h1>"+name+"</h1><h4>"+day+"</h4><input type='hidden' name=''' value='"+id+"'>";
+}
